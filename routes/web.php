c165cf9293aed7a59b65791efbbd4b724d4bae30
Physics144 <armin.kulas007@gmail.com>
2026-05-26 13:37:41 +0200

    Revert "Erstelle Controller für Dashboards (Umfang von web.php reduziert)"
    Änderungen im falschen Branch
    
    This reverts commit ad08882d825ce11cfebbba2e8e79ecce6aa12949.

--- routes/web.php
+++ routes/web.php
@@ -4,23 +4,33 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NutzerController;
 use App\Http\Controllers\ProjektController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
-use App\Http\Controllers\DashboardController;
 use App\Models\Project;
 use Illuminate\Support\Facades\Auth;
 
 // Startseite
-Route::get('/dashboard', [DashboardController::class, 'startseite']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
 
 // --- 3 ROLLEN-DASHBOARDS ---
 
 // Lehrende-Ansicht
-Route::get('/lehrende', [DashboardController::class, 'lehrendeDashboard']);
+Route::get('/lehrende', function () {
+    $projekte = Project::all();
+    return view('lehrende.dashboard', compact('projekte'));
+});
 
 // Admin-Ansicht
-Route::get('/admin', [DashboardController::class, 'adminDashboard']);
+Route::get('/admin', function () {
+    $projekte = Project::all();
+    return view('admin.dashboard', compact('projekte'));
+});
 
 // Studenten-Ansicht
-Route::get('/student', [DashboardController::class, 'studentDashboard']);
+Route::get('/student', function () {
+    $projekte = Project::where('ersteller_id', Auth::id())->get();
+    return view('student.dashboard', compact('projekte'));
+});
 
 // Startseite zeigt die Projektliste
 Route::get('/', [ProjektController::class, 'liste'])->name('projekte.liste');
@@ -43,33 +53,49 @@ Route::put('/projekte/{id}/aktualisieren', [ProjektController::class, 'aktualisi
 Route::delete('/projekte/{id}/loeschen', [ProjektController::class, 'loeschen'])->name('projekte.loeschen');
 
 // Test-Route (von Taqwa)
-// Route::get('/test', function () {
-//     return view('test');
-// });
+Route::get('/test', function () {
+    return view('test');
+});
 
 // Benutzer anlegen
 Route::get('/admin/nutzer/neu', [NutzerController::class, 'benutzerAnlegen']);
 
 Route::post('/admin/nutzer-speichern', [NutzerController::class, 'speichern']);
-
 // --- TAB-ROUTEN FÜR STUDENTEN ---
 
 // Tab 1: Alle Ideen (lädt die normale Übersicht)
-Route::get('/student/alle-ideen', [DashboardController::class, 'studentAlleProjekte']);
+Route::get('/student/alle-ideen', function () {
+   $projekte = Project::all();
+    return view('student.dashboard', compact('projekte'));
+});
 
 // Tab 2: Meine Projekte (lädt zum Testen vorerst auch das Dashboard)
-Route::get('/student/meine-projekte', [DashboardController::class, 'studentMeineProjekte']);
-Route::get('/student/nutzer/neu', [DashboardController::class, 'studentNeuesProjekt']);
+Route::get('/student/meine-projekte', function () {
+    $projekte = Project::where('ersteller_id', Auth::id())->get();
+    return view('student.dashboard', compact('projekte'));
+});
+Route::get('/student/nutzer/neu', function () {
+    // Hier kommt später das Formular hin,
+    return "Hier entsteht das Formular zum Projekte erstellen !";
+});
 
 // --- TAB-ROUTEN FÜR LEHRER ---
 // Tab 1: Alle Ideen (lädt die normale Übersicht)
-Route::get('/lehrende/alle-ideen', [DashboardController::class, 'lehrendeAlleProjekte']);
+Route::get('/lehrende/alle-ideen', function () {
+    $projekte = Project::all();
+    return view('lehrende.dashboard', compact('projekte'));
+});
 // Tab 2: Betreute Projekte (lädt zum Testen vorerst auch das Dashboard)
-Route::get('/lehrende/betreute-projekte', [DashboardController::class, 'lehrendeBetreuteProjekte']); 
-
-
+Route::get('/lehrende/betreute-projekte', function () {
+    $projekte = Project::all();
+    return view('lehrende.dashboard', compact('projekte'));
+}); 
 // 2. Admin Nutzerverwaltung (Übersicht aller Nutzer)
-Route::get('/admin/nutzer', [DashboardController::class,'adminNutzerverwaltung']);
+Route::get('/admin/nutzer', function () {
+    // Hier kann später eine Tabelle mit allen Nutzern anzeigen lassen
+    $projekte = Project::all();
+    return view('admin.dashboard', compact('projekte'));
+});
 
 
 // Login anzeigen
