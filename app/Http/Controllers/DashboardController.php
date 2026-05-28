9d3fc7c7c99664f42e72e0ac5d1c47df93ec8a58
Physics144 <armin.kulas@gmail.com>
2026-05-28 21:20:25 +0200

    Korrigieren Schreibfehler beim Namespace-Import im DashboardController

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use app\Models\Project;
+use App\Models\Project;
 use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
